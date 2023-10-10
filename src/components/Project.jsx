0f98da1d3a873654ffde600b439ddc48dcabbb1c
ericfluna <ericfernandezluna@gmail.com>
2023-10-10 17:13:55 +0200

    cat-app: implementation on colored tech labels

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,23 +1,37 @@
 import Image from "next/image";
 import { BsArrowUpRight } from "react-icons/bs";
 
-export default function Project({ image, link, icon }) {
+const techs = {
+  JavaScript: "#EFD81D",
+  React: "#61DAFB",
+};
+
+export default function Project({ image, link, icon, tecnologias }) {
   return (
-    <div className=" border-[1px] bg-[#545454] aspect-video hover:-translate-y-1 transition w-[95vw] max-w-[500px] overflow-hidden rounded-[1.5em] sm:rounded-[2.5em]">
-      <a href={link} className="overflow-hidden rounded-[1em]">
-        <Image
-          height={10000}
-          width={10000}
-          alt=""
-          src={image}
-          className={`${
-            icon ? "pt-12 px-12" : ""
-          } w-full h-[80%] aspect-video overflow-hidden rounded-t-[1em] `}
-        />
-        <div className="h-[20%] bg-[#333333] text-white text-[1.3rem] flex flex-row justify-center gap-2 items-center align-middle overflow-hidden rounded-b-[1em]">
-          Visitar <BsArrowUpRight color="#fff" size={20} />
-        </div>
-      </a>
+    <div className="space-y-2 max-w-[500px] hover:-translate-y-1 transition">
+      <div className=" border-[1px] bg-[#545454] aspect-video  w-[95vw] max-w-[500px] overflow-hidden rounded-[1.5em] sm:rounded-[2.5em]">
+        <a href={link} className="overflow-hidden rounded-[1em]">
+          <Image
+            height={10000}
+            width={10000}
+            alt=""
+            src={image}
+            className={`${
+              icon ? "pt-12 px-12" : ""
+            } w-full h-[80%] aspect-video overflow-hidden rounded-t-[1em] `}
+          />
+          <div className="h-[20%] bg-[#333333] text-white text-[1.3rem] flex flex-row justify-center gap-2 items-center align-middle overflow-hidden rounded-b-[1em]">
+            Visitar <BsArrowUpRight color="#fff" size={20} />
+          </div>
+        </a>
+      </div>
+      <div className="flex flex-row flex-wrap gap-3 items-center justify-center ">
+        {tecnologias?.map((tec, index) => (
+          <div key={index} className={`bg-[${techs[tec]}] p-3 rounded-xl`}>
+            <label>{tec}</label>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,12 @@ export default function Projects() {
       <h1 className="text-2xl lg:text-4xl font-bold break-all">PROJECTOS</h1>
 
       <div className="flex flex-row flex-wrap items-center justify-center my-10 gap-5 px-2">
-        <Project image={"/images/cat.svg"} link={"/cat-app"} icon={true} />
+        <Project
+          image={"/images/cat.svg"}
+          link={"/cat-app"}
+          icon={true}
+          tecnologias={["JavaScript", "React"]}
+        />
       </div>
     </article>
   );
